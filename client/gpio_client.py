5539db476b7f4f066c8d34c04fad0196ca4ef1a1
takado <kornelgut@gmail.com>
2023-10-30 22:57:18 +0100

    init

---
+++ client/gpio_client.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class APIClient:
+    def __init__(self):
+        self.BASE_URL = 'http://192.168.233.18:8081'
+
+    def switch_device(self, device_name):
+        endpoint = f"{self.BASE_URL}/relay/{device_name}"
+        response = requests.post(endpoint)
+        return response
+
+    def set_device_state(self, device_name, state):
+        endpoint = f"{self.BASE_URL}/relay/{device_name}/{state}"
+        response = requests.post(endpoint)
+        return response
---
+++ client/gpt_client.py
@@ -0,0 +1,78 @@
+import os
+import time
+
+import openai
+import json
+
+from client.gpio_client import APIClient
+
+
+class GPTClient:
+    def __init__(self):
+        key = os.environ['GPT_KEY']
+        openai.api_key = key
+
+    def function_call(self, message):
+        messages = [{"role": "user", "content": message}]
+        functions = [
+            {
+                "name": "switch",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "device_names": {
+                            "type": "array",
+                            "items": {
+                                "type": "string",
+                                "enum": ["lamp", "desk_lamp", "main_light"]
+                                }
+                        }
+                    },
+                    "required": ["device_names"],
+                }
+            }
+        ]
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            functions=functions,
+            function_call={"name": "switch"},
+            temperature=0
+        )
+
+        print(completion)
+        response_message = completion["choices"][0]["message"]
+        #
+        # # Step 2: check if GPT wanted to call a function
+        function_call = response_message.get("function_call")
+        if function_call:
+            arguments = function_call.get("arguments")
+            if arguments:
+                arg_dict = json.loads(arguments)
+                print(arg_dict)
+                print(type(arg_dict))
+                if 'device_names' in arg_dict:
+                    return arg_dict['device_names']
+
+
+    def send_request(self):
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system",
+                 "content": "fill in parameter and generate one line string POST request to call api endpoint: /relay/<device_name>"},
+                {"role": "user", "content": "Zgaś lampę"}
+
+            ]
+        )
+
+        print(completion)
+
+
+if __name__ == '__main__':
+    gpt = GPTClient()
+    device_names = gpt.function_call("zgaś górne światło")
+    gpio = APIClient()
+    for dev in device_names:
+        gpio.switch_device(dev)
+        time.sleep(0.5)
